8d3ee254dc758a27dc472857b9b5cc9b27be2d93
ipos# <64121677+ipostag@users.noreply.github.com>
2020-04-27 12:06:24 +0700

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -2217,7 +2217,8 @@ var cnames_active = {
   "zonayed": "zonayedpca.github.io/js.zonayed.me",
   "zplugins": "zlanguage.github.io/plugin-docs",
   "zty": "zetaoyang.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "ipostag": "ipostag.github.io/andajs"
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
